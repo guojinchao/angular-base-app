c3f59c933085903a8b85c9dc5e94a16a61f143c7
guojinchao <945723798@qq.com>
2019-01-24 15:36:27 +0800

    commit

--- src/app/admin/login-status.service.ts
+++ src/app/admin/login-status.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from "@angular/core";
 import { Router, CanActivateChild } from "@angular/router";
-import { Cookie } from "@smm/cookie";
+// import { Cookie } from "@smm/cookie";
 
 @Injectable()
 export class LoginStatusService implements CanActivateChild {
   constructor(private _route: Router) {}
 
+  IsLogin : boolean = true;
+
   canActivateChild() {
-    if (Cookie.Get("admin_login")) {
+    if (this.IsLogin) {
       return true;
     } else {
       this._route.navigateByUrl("/login");
